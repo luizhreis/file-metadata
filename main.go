ed42edad8d12f01bd6b75e2697700b71ee21d85e
Luiz H. Reis <luizhreis.gris@gmail.com>
2023-10-11 00:42:52 -0300

    feat: add sha256

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/sha256"
 	"crypto/sha512"
 	"fmt"
 	"io"
@@ -17,7 +18,9 @@ func main() {
 	}
 	defer et.Close()
 
-	fileInfos := et.ExtractMetadata("test.jpeg")
+	filePath := "test.jpeg"
+
+	fileInfos := et.ExtractMetadata(filePath)
 
 	for _, fileInfo := range fileInfos {
 		if fileInfo.Err != nil {
@@ -30,12 +33,19 @@ func main() {
 		}
 	}
 
-	sha512, err := sha512sum("test.jpeg")
+	sha512, err := sha512sum(filePath)
 	if err != nil {
 		fmt.Printf("Error when calculating sha512: %v\n", err)
 		return
 	}
 	fmt.Println("[SHA512] ", sha512)
+
+	sha256, err := sha256sum(filePath)
+	if err != nil {
+		fmt.Printf("Error when calculating sha256: %v\n", err)
+		return
+	}
+	fmt.Println("[SHA256] ", sha256)
 }
 
 func sha512sum(filePath string) (string, error) {
@@ -51,4 +61,19 @@ func sha512sum(filePath string) (string, error) {
 		return "", err
 	}
 	return fmt.Sprintf("%x", sha512.Sum(nil)), nil
+}
+
+func sha256sum(filePath string) (string, error) {
+	fileHandle, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	defer fileHandle.Close()
+
+	sha256 := sha256.New()
+	if _, err := io.Copy(sha256, fileHandle); err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", sha256.Sum(nil)), nil
 }
